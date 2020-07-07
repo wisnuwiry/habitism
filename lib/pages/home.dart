f31c52d887fdd5d850e311f73646202f3e7b71a2
12827629 <michael.k.malak@gmail.com>
2020-07-07 19:18:10 +0200

    Cleanup homepage

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -52,119 +52,74 @@ class Home extends StatelessWidget {
         return Future.value(true);
       },
       child: LayoutBuilder(builder: (context, _) {
-        final device = utils.deviceType(context);
-        if (device == utils.DeviceType.tablet) {
-          return _buildTablet;
-        }
         final orientation = MediaQuery.of(context).orientation;
-        if (orientation == Orientation.portrait) {
-          return _buildPhonePortrait;
-        } else {
-          return _buildPhoneLandscape;
+        final device = utils.deviceType(context);
+
+        if (device == utils.DeviceType.tablet ||
+            orientation == Orientation.portrait) {
+          return _buildVerticalView;
         }
+
+        return _buildHorizontalView;
       }),
     );
   }
 
-  Widget get _buildTablet => Scaffold(
-        body: DefaultTabController(
-          length: 3,
-          initialIndex: 1,
-          child: Scaffold(
-              appBar: AppBar(
-                backgroundColor: Colors.transparent,
-                elevation: 0.0,
-                flexibleSpace: Container(
-                  color: Colors.amber,
-                ),
-                title: Container(
-                  height: 36,
-                  color: Colors.transparent,
-                  child: const Text('tablet'),
-                ),
-              ),
-              body: _content(false)),
-        ),
-      );
-
-  Widget get _buildPhoneLandscape => Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-        flexibleSpace: Container(
-          color: Colors.amber,
-        ),
-        title: Container(
-          height: 36,
-          color: Colors.transparent,
-          child: const Text('PhoneLandscape'),
-        ),
-      ),
-      body: Row(
+  Widget get _buildHorizontalView => Scaffold(
+          body: Row(
         children: <Widget>[
           const SizedBox(
-              width: 150.0,
-              height: double.infinity,
-              child: SingleChildScrollView(
-                child: EmojiText('side panel goes here! $eyeEmoji'),
-              )),
+            width: 150.0,
+            height: double.infinity,
+            child: SingleChildScrollView(
+              child: EmojiText(
+                'side panel goes here! $eyeEmoji',
+              ),
+            ),
+          ),
           Expanded(child: _content(false)),
         ],
       ));
 
-  Widget get _buildPhonePortrait => Scaffold(
-//        appBar: AppBar(
-//          backgroundColor: Colors.transparent,
-//          elevation: 0.0,
-//          title: Container(
-//            height: 36,
-//            color: Colors.transparent,
-//            child: Text(
-//              DateTimeFormat.format(DateTime.now(), format: 'D, M j'),
-//            ),
-//          ),
-//        ),
-    drawer: _drawer,
-    body: _content(true),
-  );
+  Widget get _buildVerticalView => Scaffold(
+        body: _content(true),
+      );
 
-  Widget _content(bool showTitle) =>
-      Padding(
+  Widget _content(bool showDetails) => Padding(
         padding: const EdgeInsets.all(50),
         child: Column(
           children: <Widget>[
-            if (showTitle)
-              Flexible(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    const EmojiText(eyeEmoji),
-                    ResponsiveText(
-                        DateTimeFormat.format(
-                          DateTime.now(),
-                          format: 'D, M j',
-                        ),
-                        align: TextAlign.center),
-                    const EmojiText(settingsEmoji),
-                  ],
-                ),
+            Flexible(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  if (showDetails) const EmojiText(eyeEmoji),
+                  ResponsiveText(
+                      DateTimeFormat.format(
+                        DateTime.now(),
+                        format: 'D, M j',
+                      ),
+                      align: TextAlign.center),
+                  const EmojiText(settingsEmoji),
+                ],
               ),
+            ),
             Expanded(
               child: Consumer<ThemeProvider>(
-                builder: (context, themeProvider, _) =>
-                    Center(
-                      child: InkWell(
-                        onTap: () => themeProvider.toggleTheme(),
-                        child: ResponsiveText(
-                            'Click here to change the theme!'),
-                      ),
-                    ),
+                builder: (context, themeProvider, _) => Center(
+                  child: InkWell(
+                    onTap: () => themeProvider.toggleTheme(),
+                    child: ResponsiveText('Click here to change the theme!'),
+                  ),
+                ),
               ),
             ),
           ],
         ),
       );
+}
 
+//    USING CUSTOM SCROLL FOR HOME CONTENT
 //      CustomScrollView(
 //        slivers: <Widget>[
 //          SliverPadding(
@@ -210,85 +165,3 @@ class Home extends StatelessWidget {
 //          )
 //        ],
 //      );
-
-  Widget get _drawer =>
-      const Drawer(
-        child: Text('DrawerContent'),
-      );
-}
-
-//class MyHomePage extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) => Scaffold(
-//      appBar: AppBar(title: const Text('test')),
-//      drawer: MediaQuery.of(context).size.width < 500
-//          ? Drawer(
-//              child: Menu(),
-//            )
-//          : null,
-//      body: SafeArea(
-//          child: Center(
-//              child: MediaQuery.of(context).size.width < 500
-//                  ? Content()
-//                  : Row(children: [
-//                      Container(width: 200.0, child: Menu()),
-//                      Container(
-//                          width: MediaQuery.of(context).size.width - 200.0,
-//                          child: Content())
-//                    ]))));
-//}
-//
-//class Menu extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) =>
-//      ListView(children: [
-//        FlatButton(
-//            onPressed: () {},
-//            child: ListTile(
-//              leading: Icon(Icons.looks_one),
-//              title: const Text('First Link'),
-//            )),
-//        FlatButton(
-//            onPressed: () {},
-//            child: ListTile(
-//              leading: Icon(Icons.looks_two),
-//              title: const Text('Second Link'),
-//            ))
-//      ]);
-//}
-//
-//class Content extends StatelessWidget {
-//  final List<String> elements = [
-//    'Zero',
-//    'One',
-//    'Two',
-//    'Three',
-//    'Four',
-//    'Five',
-//    'Six',
-//    'Seven',
-//    'Eight',
-//    'A Million Billion Trillion',
-//    'A much, much longer text that will still fit'
-//  ];
-//
-//  @override
-//  Widget build(BuildContext context) =>
-//      GridView.builder(
-//        itemCount: elements.length,
-//        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-//          maxCrossAxisExtent: 130.0,
-//          crossAxisSpacing: 20.0,
-//          mainAxisSpacing: 20.0,
-//        ),
-//        itemBuilder: (context, i) =>
-//            Card(
-//              child: Center(
-//                child: Padding(
-//                  padding: const EdgeInsets.all(8.0),
-//                  child: Text(elements[i]),
-//                ),
-//              ),
-//            ),
-//      );
-//}
