99c7e70988201184956c79b6feb5b6f030fd99c6
12827629 <michael.k.malak@gmail.com>
2020-07-07 18:50:55 +0200

    Add dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:habitism/google_fonts_setup.dart';
-import 'package:habitism/screens/home.dart';
-import 'package:habitism/shared/themes.dart';
+import 'package:habitism/pages/home.dart';
+import 'package:habitism/utils/prefs.dart';
 
 void main() {
-//  DevicePreview(
-//    builder: (_) => MyApp(),
-//  ),
-//);
   WidgetsFlutterBinding.ensureInitialized();
+  loadPrefs();
   setupGoogleFont();
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) => MaterialApp(
-    debugShowCheckedModeBanner: false,
-        //locale: DevicePreview.of(context).locale,
-        //builder: DevicePreview.appBuilder,
-        title: 'Habitism',
-        theme: buildLightTheme(),
-        home: Home(),
-      );
+  runApp(Root());
+  //  DevicePreview(
+  //    builder: (_) => MyApp(),
+  //  ),
+  //);
 }
