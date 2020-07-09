5bd56777c86bafc3cc7df2cde76bcff06c35804c
12827629 <michael.k.malak@gmail.com>
2020-07-10 00:31:06 +0200

    Make Todos draggable

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -65,7 +65,7 @@ class Home extends StatelessWidget {
             ),
           ],
         ),
-  );
+      );
 }
 
 class HomeAppBar extends StatelessWidget {
@@ -99,14 +99,122 @@ class HomeAppBar extends StatelessWidget {
   }
 }
 
-List<String> alphabetList = [
-  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'
-];
+class HomeTodoList extends StatefulWidget {
+  @override
+  _HomeTodoListState createState() => _HomeTodoListState();
+}
 
-class HomeTodoList extends StatelessWidget {
+class _HomeTodoListState extends State<HomeTodoList> {
+  List<String> alphabetList = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J'
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ReorderableListView(
+      onReorder: _onReorder,
+      scrollDirection: Axis.vertical,
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      children: List.generate(
+        alphabetList.length,
+            (index) {
+          return ListViewCard(
+            alphabetList,
+            index,
+            Key('$index'),
+          );
+        },
+      ),
+    );
+  }
+
+  void _onReorder(int oldIndex, int newIndex) {
+    setState(
+          () {
+        if (newIndex > oldIndex) {
+          newIndex -= 1;
+        }
+        final item = alphabetList.removeAt(oldIndex);
+        alphabetList.insert(newIndex, item);
+      },
+    );
+  }
+}
+
+class ListViewCard extends StatefulWidget {
+  @override
+  final Key key;
+  final int index;
+  final List<String> listItems;
+
+  ListViewCard(this.listItems, this.index, this.key);
+
+  @override
+  _ListViewCard createState() => _ListViewCard();
+}
+
+class _ListViewCard extends State<ListViewCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(4),
+      color: Colors.white,
+      child: InkWell(
+        splashColor: Colors.blue,
+        onTap: () => print('Item ${widget.listItems[widget.index]} selected.'),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Flexible(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.all(8.0),
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Title ${widget.listItems[widget.index]}',
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 16),
+                      textAlign: TextAlign.left,
+                      maxLines: 5,
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8.0),
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Description ${widget.listItems[widget.index]}',
+                      style: const TextStyle(
+                          fontWeight: FontWeight.normal, fontSize: 16),
+                      textAlign: TextAlign.left,
+                      maxLines: 5,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+              child: Icon(
+                Icons.reorder,
+                color: Colors.grey,
+                size: 24.0,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
